e27fa3958f01ccd08e7257e316fff96e2b89e87c
Skylar Banks <skylarcbanks@gmail.com>
2025-07-20 08:36:44 -0400

    add support for consumer post confirmation

---
+++ lambdas/consumerPostConfirmationHandler.ts
@@ -0,0 +1,56 @@
+import { CognitoIdentityProviderClient, AdminInitiateAuthCommand } from '@aws-sdk/client-cognito-identity-provider'
+import type { PostConfirmationTriggerEvent } from 'aws-lambda'
+import { fetchData } from '../src/services/api'
+
+const cognitoClient = new CognitoIdentityProviderClient({ region: 'us-east-1' })
+
+const userPoolId = process.env.CONSUMER_USER_POOL_ID as string
+const clientId = process.env.CONSUMER_CLIENT_ID as string
+const username = process.env.CONSUMER_DEFAULT_USERNAME as string
+const password = process.env.CONSUMER_DEFAULT_PASSWORD as string
+
+export const handler = async (event: PostConfirmationTriggerEvent): Promise<typeof event> => {
+  try {
+    console.log('PostConfirmation Trigger Event:', JSON.stringify(event, null, 2))
+
+    const userSub = event?.request?.userAttributes?.sub
+    if (!userSub) {
+      throw new Error("User 'sub' not found in attributes")
+    }
+
+    const authResponse = await cognitoClient.send(
+      new AdminInitiateAuthCommand({
+        AuthFlow: 'ADMIN_USER_PASSWORD_AUTH',
+        UserPoolId: userPoolId,
+        ClientId: clientId,
+        AuthParameters: {
+          USERNAME: username,
+          PASSWORD: password,
+        },
+      })
+    )
+    const sessionToken = authResponse.AuthenticationResult?.AccessToken
+    if (!sessionToken) {
+      throw new Error('Failed to retrieve session token')
+    }
+    await fetchData({
+      url: '/user',
+      method: 'POST',
+      payload: {
+        authId: userSub,
+        account: {
+          email: event?.request?.userAttributes?.email,
+          type: 'REGISTERED',
+          profile: {},
+          carts: [{}]
+        }
+      },
+      token: sessionToken,
+    })
+
+    return event
+  } catch (error) {
+    console.error('PostConfirmation Error:', error)
+    throw error
+  }
+}
